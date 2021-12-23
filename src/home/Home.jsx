1428a53891c44bb4cd17e26b6f0c4840980e35e3
marizoo <marizoo@meow.woof>
2021-12-23 20:53:25 +0700

    component List.jsx and ListItem.jsx done

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar'
-import Featured from '../featured/Featured'
+import Featured from '../components/featured/Featured'
 import './home.scss'
+import List from '../components/list/List'
 
 const Home = () => {
     return (
         <div className='home'>
             <Navbar />
-           
             <Featured/>
+            <List />
+            <List />
+            <List />
          </div>
     )
 }
