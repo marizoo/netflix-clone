99c0c9daf598a1e836c5a71c76ba44e799f3e782
marizoo <marizoo@meow.woof>
2021-12-23 23:10:03 +0700

    completed the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import './app.scss'
-import Home from './home/Home'
+import Home from './pages/home/Home'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Watch from './pages/watch/Watch'
+import Register from './pages/register/Register'
+import Login from './pages/login/Login'
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/watch' element={<Watch />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+      
+    </Router>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Navbar from '../components/navbar/Navbar'
-import Featured from '../components/featured/Featured'
+import Navbar from '../../components/navbar/Navbar'
+import Featured from '../../components/featured/Featured'
 import './home.scss'
-import List from '../components/list/List'
+import List from '../../components/list/List'
 
 const Home = () => {
     return (
